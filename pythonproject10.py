6dfe3b72aca0b3011385fb1e73250433b1f2edd3
kanansnote <99095303+kanansnote@users.noreply.github.com>
2022-10-24 01:55:41 +0400

    Add files via upload

---
+++ pythonproject10.py
@@ -0,0 +1,13 @@
+# New Product Rating
+
+flavors = ["cinnamon", "pumpkin", "apple pie"]
+print("New flavors:")
+print(flavors)
+
+ratings = [4, 2.5, 3]
+print("Consumer ratings:")
+print(ratings)
+
+passed = [True, False, True]
+print("Release:")
+print(passed)
