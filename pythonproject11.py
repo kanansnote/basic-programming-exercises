bcadadccb77de59214779201622f6af782dfb1cf
kanansnote <99095303+kanansnote@users.noreply.github.com>
2022-10-24 11:45:35 +0400

    Create pythonproject11.py

---
+++ pythonproject11.py
@@ -0,0 +1,14 @@
+# Stocks Tracker
+
+apple_stocks = [298.18, 304.18, 289.23]
+
+apple_stocks[0] = 310
+print("latest value:")
+print(apple_stocks[0])
+
+apple_stocks[1] = 310
+print("highest value:")
+print(apple_stocks[1])
+
+print("lowest value:")
+print(apple_stocks[2])
