311572c5d96b446025e63c7c68ac73ab4c11006f
kanansnote <99095303+kanansnote@users.noreply.github.com>
2022-10-28 02:34:23 +0400

    Create pythonproject12.py

---
+++ pythonproject12.py
@@ -0,0 +1,19 @@
+# Customers Service Queue System
+
+customers = ["Filip", "Miranda", "Ariah", "Laurence", "Shahid"]
+
+free_counters = [11, 20, 14, 6, 4]
+
+customers.sort()
+
+free_counters.sort()
+
+customer = customers[0]
+
+counter = free_counters[0]
+
+print(customer)
+print("Visit counter:")
+print(counter)
+
+# Created: Oct 28, 2022
