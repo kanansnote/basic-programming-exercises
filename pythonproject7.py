d6fae31793e7f7752ecb084940d181b8aeb60847
kanansnote <99095303+kanansnote@users.noreply.github.com>
2022-10-28 14:18:00 +0400

    Update pythonproject7.py

--- pythonproject7.py
+++ pythonproject7.py
@@ -1,3 +1,5 @@
+# Compound Interest Calculator
+
 account = 100
 interest_rate = 0.004
 years = 3
@@ -10,3 +12,5 @@ while counter <= years:
     account += accrued_interest
     print(f"year {counter}:{account}")
     counter += 1
+
+# Oct 20, 2022
