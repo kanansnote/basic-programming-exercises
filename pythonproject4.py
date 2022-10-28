d7fe1a161e958d58221b82f0b01170010bb184e5
kanansnote <99095303+kanansnote@users.noreply.github.com>
2022-10-28 14:13:21 +0400

    Update pythonproject4.py

--- pythonproject4.py
+++ pythonproject4.py
@@ -1,3 +1,5 @@
+# Track Sales Data
+
 stock = 600
 jeans_sold = 500
 target = 500
@@ -10,3 +12,5 @@ current_stock = stock - jeans_sold
 in_stock = current_stock != 0
 print("Jeans in stock:")
 print(in_stock)
+
+# Oct 16, 2022
