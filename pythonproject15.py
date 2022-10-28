9264e80e7572e2ff8de7dba9dd3ff738762009ab
kanansnote <99095303+kanansnote@users.noreply.github.com>
2022-10-28 17:35:21 +0400

    Create pythonproject15.py

---
+++ pythonproject15.py
@@ -0,0 +1,17 @@
+# Product Ratings Analysis
+
+ratings = [5, 5, 3, 1, 1, 1, 4, 5, 3, 5]
+
+five_star = ratings.count(5)
+
+one_star = ratings.count(1)
+
+amounts = [five_star, one_star]
+
+print("Five star ratings:")
+print(amounts[0])
+
+print("One star ratings:")
+print(amounts[1])
+
+# Oct 28, 2022
