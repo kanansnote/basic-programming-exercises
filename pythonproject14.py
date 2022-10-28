3c28ce4b0ee51576bed68ed36094d7792315bc31
kanansnote <99095303+kanansnote@users.noreply.github.com>
2022-10-28 17:18:59 +0400

    Create pythonproject14.py

---
+++ pythonproject14.py
@@ -0,0 +1,19 @@
+# Lake Size Comparison
+
+sizes = [7340, 2117, 22300, 31700, 1679, 1014, 23000, 9910, 685]
+
+largest = max(sizes)
+
+print("Largest lake in sq mi:")
+print(largest)
+
+smallest = min(sizes)
+
+print("Smallest lake in sq mi:")
+print(smallest)
+
+difference = largest - smallest
+print("Difference between lakes in sq mi:")
+print(difference)
+
+# Oct 28, 2022
