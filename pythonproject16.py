4d6384b3f224ac9ee3761b04a02141facb2a4b9b
kanansnote <99095303+kanansnote@users.noreply.github.com>
2022-10-29 23:07:45 +0400

    Create pythonproject16.py

---
+++ pythonproject16.py
@@ -0,0 +1,11 @@
+# Stock Manager
+
+stock = 74
+item = "webcams"
+orders = 18
+
+print(f"{stock}{items} in stock")
+print(f"{orders} customer orders")
+print(f"{stock - orders} available")
+
+# Oct 29, 2022
