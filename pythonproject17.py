82ab34db22479774ee9bc11f8a3690b91c115799
kanansnote <99095303+kanansnote@users.noreply.github.com>
2022-10-29 23:53:22 +0400

    Create pythonproject17.py

---
+++ pythonproject17.py
@@ -0,0 +1,23 @@
+# Game Data
+
+player_1 = "Sam"
+player_2 = "Alex"
+current_round = 1
+
+print("Game on!")
+print(f"Player 1: {player_1}")
+print(f"Player 2: {player_2}")
+print("-----------------------------")
+
+print(f"Round {current_round}!")
+player_1_score = 10
+player_2_score = 13
+print(f"{player_2} wins with {player_2_score} to {player_1_score}")
+print("-----------------------------")
+
+print(f"Round {current_round + 1}!")
+player_1_score = 20
+player_2_score = 5
+print(f"{player_1} defeats {player_2} by {player_1_score - player_2_score} points")
+
+# Oct 29, 2022
