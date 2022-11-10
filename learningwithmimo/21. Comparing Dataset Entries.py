3fdee17eed5a20ad6e6f29efe6148fb7b48bf09a
kanansnote <99095303+kanansnote@users.noreply.github.com>
2022-11-11 01:11:40 +0400

    Update 21. Comparing Dataset Entries.py

--- learningwithmimo/21. Comparing Dataset Entries.py
+++ learningwithmimo/21. Comparing Dataset Entries.py
@@ -8,4 +8,14 @@ id_2 = "#5"
 average_grade_2 = "A"
 test_score_2 = 70
 
+no_duplicates = id_1 != id_2
+print("No duplicate entries:")
+print(no_duplicates)
+
+same_average = average_grade_1 == average_grade_2
+print(f"Same average grade: {same_average}")
+
+higher_score = test_score_1 > test_score_2
+print(f"id_1 has a higher score: {higher_score}")
+
 # Nov 11, 2022
