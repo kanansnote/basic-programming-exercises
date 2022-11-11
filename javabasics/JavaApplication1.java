82940c6042dde2d34a3c18eb933df7edc49e5958
kanansnote <99095303+kanansnote@users.noreply.github.com>
2022-11-12 00:01:17 +0400

    Create JavaApplication1.java

---
+++ javabasics/JavaApplication1.java
@@ -0,0 +1,11 @@
+package javaapplication1;
+
+class ForDemo {
+    public static void main (String [] args) {
+        for(int n=1; n<=100; n++){
+            int s = 0;
+            int x = 1;
+            System.out.println("s=5*x*x*12");
+        }
+    }
+}
