503db6976d812bcd95c9e3e56fd3a4a086bf8bee
kanansnote <99095303+kanansnote@users.noreply.github.com>
2022-11-12 00:16:41 +0400

    Create devcplusproject1.cpp

---
+++ cppbasics/devcplusproject1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	int x,y,z;
+	cout<<"Birinci ededi daxil edin \n";
+	cin>>x;
+	cout<<"Ikinci ededi daxil edin \n";
+	cin>>y;
+	z=x+y;
+	cout<<"Cavab = ";
+	cout<<z;
+	z=x-y;
+	cout<<"\nCavab = ";
+	cout<<z;
+	cout<<"\nCavab = ";
+	cout<<z/2;
+	z=x*y;
+	cout<<"\nCavab = ";
+	cout<<z;	
+}
