38ecf150490fc4b4a5ac3c5b6735fde908fff33f
kanansnote <99095303+kanansnote@users.noreply.github.com>
2022-11-12 00:18:04 +0400

    Create newprojectafteryears1.cpp

---
+++ cppbasics/newprojectafteryears1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	float annualSalary;
+	cout << "Enter your number\n";
+	cin >> annualSalary;
+	float monthlySalary = annualSalary / 12;
+	cout << "Your monthly salary is " << monthlySalary << endl;
+	cout << "In 10 years you will earn " << annualSalary * 10;
+	system("pause>0");
+}
