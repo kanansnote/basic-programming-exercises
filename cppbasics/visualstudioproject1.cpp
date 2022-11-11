5f8f347f9cfe5b66e4e367dc7059be968abd2730
kanansnote <99095303+kanansnote@users.noreply.github.com>
2022-11-12 00:19:06 +0400

    Create visualstudioproject1.cpp

---
+++ cppbasics/visualstudioproject1.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// We can put commentaries in this section.
+    /* Also 
+    	Here :D */
+	cout << "Hello there \n" << "That's awesome!";
+	return 0;
+}
