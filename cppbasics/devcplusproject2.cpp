0f159fdeaae818c772a6c485455f79b4d12265b3
kanansnote <99095303+kanansnote@users.noreply.github.com>
2022-11-12 00:17:22 +0400

    Create devcplusproject2.cpp

---
+++ cppbasics/devcplusproject2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	int x,y,z;
+	cout<<"Xosh gorduk. Gelin deyishenlere verilecek qiymetlerle bir sira emelleri yerine yetirek.";
+	x=5;
+	y=2;
+	z=x+y;
+	cout<<"\nEdedleri topladiq."<<" Cavab = ";
+	cout<<z;
+	z=x-y;
+	cout<<"\nBirinci ededden ikinci ededi chixdiq."<<" Cavab = ";
+	cout<<z;
+	cout<<"\nChixma emelinden aldighimiz cavabi beshe bolduk."<<" Cavab = ";
+	cout<<z/5;
+	z=x/y;
+	cout<<"\nBirinci ededi ikinci edede bolduk."<<" Cavab = ";
+	cout<<z;
+	z=x*y;
+	cout<<"\nEdedleri bir-birine vurduq."<<" Cavab = ";
+	cout<<z;
+	cout<<"\n(x+y)/(x-y) ifadesini hell etdik."<<" Cavab = ";
+	cout<<(x+y)/(x-y);
+}
