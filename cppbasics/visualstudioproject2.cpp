d81d219735bd40ca1fd5d3e760439929048119a1
kanansnote <99095303+kanansnote@users.noreply.github.com>
2022-11-12 00:19:38 +0400

    Create visualstudioproject2.cpp

---
+++ cppbasics/visualstudioproject2.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, sum;
+	cout << "Enter your first number\n";
+	cin >> a;
+	cout << "Enter your second number\n";
+	cin >> b;
+	sum = a + b;
+	cout << "Sum is: " << sum;
+	
+	return 0;
+}
