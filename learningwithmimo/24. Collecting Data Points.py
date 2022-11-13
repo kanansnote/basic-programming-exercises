ce5b9201bbd13b8872cf97c68f9a3f1580b75675
kanansnote <99095303+kanansnote@users.noreply.github.com>
2022-11-14 02:11:44 +0400

    Add files via upload

---
+++ learningwithmimo/24. Collecting Data Points.py
@@ -0,0 +1,11 @@
+# Collecting Data Points
+
+minimum_points = 100
+data_points = 150
+
+if data_points >= minimum_points:
+    print("There are enough data points!")
+if data_points < minimum_points:
+    print('Keep collecting data.')
+
+# Nov 14, 2022
