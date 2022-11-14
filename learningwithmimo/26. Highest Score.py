0a596a136da7c52b353df911c6c61be355a80ec4
kanansnote <99095303+kanansnote@users.noreply.github.com>
2022-11-15 02:09:18 +0400

    Add files via upload

---
+++ learningwithmimo/26. Highest Score.py
@@ -0,0 +1,12 @@
+# Highest Score
+
+user_scores = [12, 42, 55, 100, 11, 22]
+highest = user_scores[0]
+
+for score in user_scores:
+    if score > highest:
+        highest = score
+
+print(f"Highest score: {highest}")
+
+# Nov 15, 2022
---
+++ learningwithmimo/27. Tip Calculator.py
@@ -0,0 +1,16 @@
+# Tip Calculator
+
+bill = 20
+tip_percentage = 0.15
+tax_percentage = 0.067
+
+tip = bill * tip_percentage
+print(f"Tip: {tip}")
+
+tax = bill * tax_percentage
+print(f"Tax: {tax}")
+
+total = bill + tip + tax
+print(f"Total: {total}")
+
+# Nov 15, 2022
