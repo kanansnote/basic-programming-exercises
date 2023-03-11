e5436aca457f55a4035009775f96b1e6ea1e8772
kanansnote <99095303+kanansnote@users.noreply.github.com>
2023-03-11 23:52:58 +0400

    Update devcplusproject2.cpp

--- cppbasics/devcplusproject2.cpp
+++ cppbasics/devcplusproject2.cpp
@@ -1,4 +1,4 @@
-// Created: Jan 27, 2017 | Edited: Mar 11, 2023
+// Arithmetic Operations 2
 
 #include <iostream>
 using namespace std;
@@ -9,19 +9,21 @@ int main ()
 	x = 5;
 	y = 2;
 	z = x + y;
-	cout << "\nEdedleri topladiq."<<" Cavab = ";
+	cout << "\nEdedleri topladiq." << " Cavab = ";
 	cout << z;
-	z = x-y;
-	cout << "\nBirinci ededden ikinci ededi chixdiq."<<" Cavab = ";
+	z = x - y;
+	cout << "\nBirinci ededden ikinci ededi chixdiq." << " Cavab = ";
 	cout << z;
-	cout << "\nChixma emelinden aldighimiz cavabi beshe bolduk."<<" Cavab = ";
-	cout << z/5;
-	z = x/y;
-	cout << "\nBirinci ededi ikinci edede bolduk."<<" Cavab = ";
+	cout << "\nChixma emelinden aldighimiz cavabi beshe bolduk." << " Cavab = ";
+	cout << z / 5;
+	z = x / y;
+	cout << "\nBirinci ededi ikinci edede bolduk." << " Cavab = ";
 	cout << z;
 	z = x * y;
-	cout << "\nEdedleri bir-birine vurduq."<<" Cavab = ";
+	cout << "\nEdedleri bir-birine vurduq." << " Cavab = ";
 	cout << z;
-	cout << "\n(x+y)/(x-y) ifadesini hell etdik."<<" Cavab = ";
+	cout << "\n(x + y) / (x - y) ifadesini hell etdik." << " Cavab = ";
 	cout << (x + y) / (x - y);
 }
+
+// Created: Jan 27, 2017 | Edited: Mar 11, 2023
