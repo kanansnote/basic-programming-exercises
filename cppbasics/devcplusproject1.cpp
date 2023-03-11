ef60f36f26a108cd88d8c9b1dcb3559b8c258948
kanansnote <99095303+kanansnote@users.noreply.github.com>
2023-03-11 23:52:51 +0400

    Update devcplusproject1.cpp

--- cppbasics/devcplusproject1.cpp
+++ cppbasics/devcplusproject1.cpp
@@ -1,23 +1,25 @@
-// Created: Jan 26, 2017 | Edited: Feb 06, 2022
+// Arithmetic Operations 1
+
 #include <iostream>
-
 using namespace std;
-
-int main (){
-	int x,y,z;
-	cout<<"Birinci ededi daxil edin \n";
-	cin>>x;
-	cout<<"Ikinci ededi daxil edin \n";
-	cin>>y;
-	z=x+y;
-	cout<<"Cavab = ";
-	cout<<z;
-	z=x-y;
-	cout<<"\nCavab = ";
-	cout<<z;
-	cout<<"\nCavab = ";
-	cout<<z/2;
-	z=x*y;
-	cout<<"\nCavab = ";
-	cout<<z;	
+int main ()
+{
+	int x, y, z;
+	cout << "Birinci ededi daxil edin \n";
+	cin >> x;
+	cout << "Ikinci ededi daxil edin \n";
+	cin >> y;
+	z = x + y;
+	cout << "Cavab = ";
+	cout << z;
+	z = x - y;
+	cout << "\nCavab = ";
+	cout << z;
+	cout << "\nCavab = ";
+	cout << z / 2;
+	z = x * y;
+	cout << "\nCavab = ";
+	cout << z;	
 }
+
+// Created: Jan 26, 2017 | Edited: Mar 11, 2023
