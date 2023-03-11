57971c0855cb5906e8599e6bac68b35dcb275b65
kanansnote <99095303+kanansnote@users.noreply.github.com>
2023-03-11 23:58:40 +0400

    Update visualstudioproject1.cpp

--- cppbasics/visualstudioproject1.cpp
+++ cppbasics/visualstudioproject1.cpp
@@ -1,4 +1,4 @@
-// Basic Line (English)
+// Simple Text Output (English)
 
 #include "pch.h"
 #include <iostream>
@@ -7,8 +7,8 @@ using namespace std;
 int main()
 {
 	// We can put commentaries in this section.
-    /* Also 
-    	Here :D */
+	/* Also 
+    	Here :) */
 	cout << "Hello there \n" << "That's awesome!";
 	return 0;
 }
