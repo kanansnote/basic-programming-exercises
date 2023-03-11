14e3cf2653d5679d27020426e870c77e4f8874d1
kanansnote <99095303+kanansnote@users.noreply.github.com>
2023-03-12 00:16:23 +0400

    Update and rename 1. A Mathematical Operation.java to javaapp.java

--- javabasics/javaapp.java
+++ javabasics/javaapp.java
@@ -1,11 +1,15 @@
+// Arithmetic Operation
+
 package javaapplication1;
 
 class ForDemo {
     public static void main (String [] args) {
-        for(int n=1; n<=100; n++){
+        for(int n = 1; n <= 100; n++){
             int s = 0;
             int x = 1;
             System.out.println("s=5*x*x*12");
         }
     }
 }
+
+// Oct 16, 2018 | Mar 12, 2023
