b3d4d0030488c9365471476438298e049c495ba6
Kanan N <kanansnote@gmail.com>
2023-09-30 19:06:10 +0400

    Create 5-soccerteam.js

---
+++ learningwithmimo/learningjavascript/5-soccerteam.js
@@ -0,0 +1,13 @@
+// Soccer Team - Sept 30, 2023
+
+const goalie = ["Sam"];
+console.log("Goalie: " + goalie);
+
+const defenders = ["Lisa", "Tom", "Jenny", "Jo"];
+console.log("Defenders: " + defenders);
+
+const midfielders = ["Jim", "Kai", "Ian", "Ava"];
+console.log("Midfielders: " + midfielders);
+
+const forwards = ["Kim", "Lucas"];
+console.log("Forwards: " + forwards);
